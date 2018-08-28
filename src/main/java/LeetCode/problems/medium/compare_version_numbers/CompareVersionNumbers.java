f41975361a404ba81692f8b7516b44a5f8f7a58d
Grigory <gtukmachev@gmail.com>
2018-08-28 19:39:51 +0700

    Compare Version Numbers

--- src/main/java/LeetCode/problems/medium/compare_version_numbers/CompareVersionNumbers.java
+++ src/main/java/LeetCode/problems/medium/compare_version_numbers/CompareVersionNumbers.java
@@ -1,5 +1,7 @@
 package LeetCode.problems.medium.compare_version_numbers;
 
+import java.util.Iterator;
+
 /**
  * Created by grigory@clearscale.net on 8/28/2018.
  */
@@ -8,9 +10,67 @@ public class CompareVersionNumbers {
     public class Solution {
 
         public int compareVersion(String version1, String version2) {
+            Iterator<String> v1Iterator = new VersionIterator(version1);
+            Iterator<String> v2Iterator = new VersionIterator(version2);
+
+            for(;;){
+                String v1 = v1Iterator.next();
+                String v2 = v2Iterator.next();
+
+                if (v1 == null && v2 == null) return  0;
+                if (v1 != null && v2 == null) return  1;
+                if (v1 == null /*&& v2 != null*/) return -1;
+
+                int r = compare(v1, v2);
+                if (r != 0) return r;
+            }
+
+        }
+
+        int compare(String v1, String v2) {
+            int l1 = v1.length() - 1;
+            int l2 = v2.length() - 1;
+
+            if (l1 > l2) return  1;
+            if (l1 < l2) return -1;
+
+            while (v1.charAt(l1) == v2.charAt(l1) && l1 > 0 ) l1--;
+
+            if (v1.charAt(l1) > v2.charAt(l1)) return  1;
+            if (v1.charAt(l1) < v2.charAt(l1)) return -1;
             return 0;
         }
+
     }
+
+    public static class VersionIterator implements Iterator<String> {
+        String s;
+        int p = 0;
+
+        public VersionIterator(String s) { this.s = s; }
+
+        @Override
+        public boolean hasNext() {
+            return p >= 0;
+        }
+
+        @Override
+        public String next() {
+            if (p == -1) return null;
+            int n = s.indexOf('.', p);
+            String r;
+            if (n == -1) {
+                r = s.substring(p);
+                p = -1;
+            } else {
+                r = s.substring(p, n);
+                p = n+1;
+            }
+
+            return r;
+        }
+    }
+
 
 
 }
--- src/main/java/LeetCode/problems/medium/compare_version_numbers/Tests.java
+++ src/main/java/LeetCode/problems/medium/compare_version_numbers/Tests.java
@@ -2,6 +2,7 @@ package LeetCode.problems.medium.compare_version_numbers;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertNull;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,6 +12,17 @@ import static org.junit.Assert.assertThat;
 public class Tests {
 
     CompareVersionNumbers.Solution s = new CompareVersionNumbers().new Solution();
+
+    @Test public void tt_iterator_1() {
+        CompareVersionNumbers.VersionIterator it = new CompareVersionNumbers.VersionIterator("11.22.333..4.5");
+        assertThat(it.next(), is("11"));
+        assertThat(it.next(), is("22"));
+        assertThat(it.next(), is("333"));
+        assertThat(it.next(), is(""));
+        assertThat(it.next(), is("4"));
+        assertThat(it.next(), is("5"));
+        assertNull(it.next());
+    }
 
 
     @Test public void t0(){ assertThat(s.compareVersion("1", "1"), is( 0)); }
